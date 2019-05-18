e70f6cc2a0bbc4cde2151da5f9e7d603126fc765
nikitarub <nikita.rub@yandex.ru>
2019-05-18 13:36:47 +0300

    Fixed meta and turnd debugger off bu default

--- bus.js
+++ bus.js
@@ -14,7 +14,7 @@ const _meta = {};
  * @param mode {Boolean} – toggler of debug mode
  */
 const mode = {
-    debug: true
+    debug: false
 };
 
 
@@ -83,7 +83,7 @@ function _encode(from, event) {
 */
 const _createMeta = ({from, link, event, callback, data} = {}) => {
     return {
-        'from': from.name ? from.name : undefined,
+        'from': from ? from.name : undefined,
         'link': link ? link : undefined,
         'event': event ? event : undefined,
         'callback': callback ? callback.name : undefined,
